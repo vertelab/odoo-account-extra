7605297a141783f99974d4acfdb7f13e6ad39209
anderswallenquist <anders.wallenquist@vertel.se>
2015-08-10 11:40:43 +0200

    Form updated. Url for download of files

--- website_project_issue/project_issue.py
+++ website_project_issue/project_issue.py
@@ -26,9 +26,27 @@ from openerp import SUPERUSER_ID
 from datetime import datetime
 import werkzeug
 import pytz
+import base64
+from openerp.tools import ustr
+import urllib2
+
 
 import logging
 _logger = logging.getLogger(__name__)
+
+
+
+def content_disposition(filename):
+    filename = ustr(filename)
+    escaped = urllib2.quote(filename.encode('utf8'))
+    browser = request.httprequest.user_agent.browser
+    version = int((request.httprequest.user_agent.version or '0').split('.')[0])
+    if browser == 'msie' and version < 9:
+        return "attachment; filename=%s" % escaped
+    elif browser == 'safari':
+        return u"attachment; filename=%s" % filename
+    else:
+        return "attachment; filename*=UTF-8''%s" % escaped
 
 class website_project_issue(http.Controller):
         
@@ -36,39 +54,36 @@ class website_project_issue(http.Controller):
     def upload_attachement(self, issue=False, **post):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         error = {}
-        default = {}
-        if not issue:
-            issue = request.env['ir.module.module'].create({'user_id': uid})
-
-        _logger.error("website_project_issue-module issue %s /issue/nn" % (issue))
-
+        user = request.env['res.users'].browse(uid)
+        if not issue and request.httprequest.method == 'POST':
+            issue = request.env['project.issue'].create({'partner_id': user.partner_id.id, 'name': post.get('name'), 'description': post.get('description')})
+        if issue and request.httprequest.method == 'POST':
+            issue.write({'partner_id': user.partner_id.id, 'name': post.get('name'), 'description': post.get('description')})
         
-        if request.httprequest.method == 'POST' and post['ufile']:
-            _logger.error("This is attachement post %s /issue/nn" % (post))
-            env['ir.attachment'].create({
+        if request.httprequest.method == 'POST' and post.get('ufile'):
+            _logger.debug("This is attachement post %s /issue/nn" % (post))
+            request.env['ir.attachment'].create({
                     'name': post['ufile'].filename,
                     'res_name': issue.name,
                     'res_model': 'project.issue',
                     'res_id': issue.id,
                     'datas': base64.encodestring(post['ufile'].read()),
                     'datas_fname': post['ufile'].filename,
-                })   
+                })
                 
-
+        
         return request.website.render("website_project_issue.upload_attachement", {
                 'issue': issue,
-                'post': post,
-                'self': self,
                 'error': error,
-                'default': default,
+                'attachements': issue and request.env['ir.attachment'].search([('res_model','=','project.issue'),('res_id','=',issue.id)]) or False,
             })
         
-        #~ if module:
-            #~ module_obj = request.env['ir.module.module'].search([('name','=',module)])[0]
-        return 
-
-
-#~ class product_template(models.Model):
-    #~ _inherit = 'product.template'
-    #~ 
-    #~ quote_module_id = fields.Many2one('ir.module.module', string='Module')
+        
+    @http.route(['/file/<model("ir.attachment"):file>',], type='http', auth='user')
+    def file_download(self, file=False, **kw):
+        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
+        
+        return request.make_response(base64.b64decode(file.datas),
+                [('Content-Type', file.mimetype),
+                 ('Content-Disposition', content_disposition(file.name))])
+  
