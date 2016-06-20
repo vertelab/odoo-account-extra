a81ae5c3cdd90278faae130a50e6823b00b28d8b
Anders Wallenquist <aw@leia.vertel.se>
2016-06-20 11:15:48 +0200

    account_writeoff_fix: new module, with correct calculation of writeoff_amount

---
+++ account_writeoff_fix/account_voucher.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2004-2016 Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+import time
+from lxml import etree
+
+from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
+from openerp.tools.translate import _
+from openerp.tools import float_compare
+from openerp.report import report_sxw
+import openerp
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class account_voucher(osv.osv):
+
+    # This is a copy of account_voucher._compute_writeoff_amount
+    def _compute_writeoff_amount(self, cr, uid, line_dr_ids, line_cr_ids, amount, type):
+        debit = credit = 0.0
+        sign = type == 'payment' and -1 or 1
+        for l in line_dr_ids:
+            if isinstance(l, dict):
+                debit += l['amount_unreconciled']  # amount -> amount_unreconciled
+        for l in line_cr_ids:
+            if isinstance(l, dict):
+                credit += l['amount_unreconciled'] # amount -> amount_unreconciled
+        return amount - sign * (credit - debit)
+
+
