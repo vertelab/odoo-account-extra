228a7fc08394ef10dbf8373595125e665468e856
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-09-01 11:06:51 +0200

    account_voucher_tax: import dp

--- account_voucher_tax/account_voucher.py
+++ account_voucher_tax/account_voucher.py
@@ -22,6 +22,9 @@ from openerp import models, fields, api, _
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 import openerp.tools
 
+from openerp.tools import float_compare
+import openerp.addons.decimal_precision as dp
+
 import logging
 _logger = logging.getLogger(__name__)
 
