4c09d02f28848a73966ef6c2286ad747983c2a87
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-09-01 10:21:05 +0200

    account_voucher_tax: new module account_voucher_tax#   (använd "git pull" för att uppdatera din lokala gren)

---
+++ account_voucher_tax/account_voucher.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2016 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+import openerp.tools
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class account_voucher(models.Model):
+    _inherit = 'account.voucher'
+    
+    tax_line = fields.One2many('account.voucher.tax', 'voucher_id', string='Tax Lines',
+        readonly=True, states={'draft': [('readonly', False)]}, copy=True)
+    
+    
+class account_voucher_line(models.Model):
+    _inherit = 'account.voucher.line'
+    
+    invoice_line_tax_id = fields.Many2many('account.tax','account_voucher_line_tax', 'voucher_line_id', 'tax_id',
+        string='Taxes', domain=[('parent_id', '=', False), '|', ('active', '=', False), ('active', '=', True)])
+    
+class account_voucher_tax(models.Model):
+    _name = "account.voucher.tax"
+    _description = "Voucher Tax"
+    _order = 'sequence'
+
+    @api.one
+    @api.depends('base', 'base_amount', 'amount', 'tax_amount')
+    def _compute_factors(self):
+        self.factor_base = self.base_amount / self.base if self.base else 1.0
+        self.factor_tax = self.tax_amount / self.amount if self.amount else 1.0
+
+    voucher_id = fields.Many2one('account.voucher', string='Invoice Line',
+        ondelete='cascade', index=True)
+    name = fields.Char(string='Tax Description',
+        required=True)
+    account_id = fields.Many2one('account.account', string='Tax Account',
+        required=True, domain=[('type', 'not in', ['view', 'income', 'closed'])])
+    account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic account')
+    base = fields.Float(string='Base', digits=dp.get_precision('Account'))
+    amount = fields.Float(string='Amount', digits=dp.get_precision('Account'))
+    manual = fields.Boolean(string='Manual', default=True)
+    sequence = fields.Integer(string='Sequence',
+        help="Gives the sequence order when displaying a list of invoice tax.")
+    base_code_id = fields.Many2one('account.tax.code', string='Base Code',
+        help="The account basis of the tax declaration.")
+    base_amount = fields.Float(string='Base Code Amount', digits=dp.get_precision('Account'),
+        default=0.0)
+    tax_code_id = fields.Many2one('account.tax.code', string='Tax Code',
+        help="The tax basis of the tax declaration.")
+    tax_amount = fields.Float(string='Tax Code Amount', digits=dp.get_precision('Account'),
+        default=0.0)
+
+    company_id = fields.Many2one('res.company', string='Company',
+        related='account_id.company_id', store=True, readonly=True)
+    factor_base = fields.Float(string='Multipication factor for Base code',
+        compute='_compute_factors')
+    factor_tax = fields.Float(string='Multipication factor Tax code',
+        compute='_compute_factors')
+
+    @api.multi
+    def base_change(self, base, currency_id=False, company_id=False, date_invoice=False):
+        factor = self.factor_base if self else 1
+        company = self.env['res.company'].browse(company_id)
+        if currency_id and company.currency_id:
+            currency = self.env['res.currency'].browse(currency_id)
+            currency = currency.with_context(date=date_invoice or fields.Date.context_today(self))
+            base = currency.compute(base * factor, company.currency_id, round=False)
+        return {'value': {'base_amount': base}}
+
+    @api.multi
+    def amount_change(self, amount, currency_id=False, company_id=False, date_invoice=False):
+        company = self.env['res.company'].browse(company_id)
+        if currency_id and company.currency_id:
+            currency = self.env['res.currency'].browse(currency_id)
+            currency = currency.with_context(date=date_invoice or fields.Date.context_today(self))
+            amount = currency.compute(amount, company.currency_id, round=False)
+        tax_sign = (self.tax_amount / self.amount) if self.amount else 1
+        return {'value': {'tax_amount': amount * tax_sign}}
+
+    @api.v8
+    def compute(self, invoice):
+        tax_grouped = {}
+        currency = invoice.currency_id.with_context(date=invoice.date_invoice or fields.Date.context_today(invoice))
+        company_currency = invoice.company_id.currency_id
+        for line in invoice.invoice_line:
+            taxes = line.invoice_line_tax_id.compute_all(
+                (line.price_unit * (1 - (line.discount or 0.0) / 100.0)),
+                line.quantity, line.product_id, invoice.partner_id)['taxes']
+            for tax in taxes:
+                val = {
+                    'voucher_id': voucher.id,
+                    'name': tax['name'],
+                    'amount': tax['amount'],
+                    'manual': False,
+                    'sequence': tax['sequence'],
+                    'base': currency.round(tax['price_unit'] * line['quantity']),
+                }
+                if invoice.type in ('out_invoice','in_invoice'):
+                    val['base_code_id'] = tax['base_code_id']
+                    val['tax_code_id'] = tax['tax_code_id']
+                    val['base_amount'] = currency.compute(val['base'] * tax['base_sign'], company_currency, round=False)
+                    val['tax_amount'] = currency.compute(val['amount'] * tax['tax_sign'], company_currency, round=False)
+                    val['account_id'] = tax['account_collected_id'] or line.account_id.id
+                    val['account_analytic_id'] = tax['account_analytic_collected_id']
+                else:
+                    val['base_code_id'] = tax['ref_base_code_id']
+                    val['tax_code_id'] = tax['ref_tax_code_id']
+                    val['base_amount'] = currency.compute(val['base'] * tax['ref_base_sign'], company_currency, round=False)
+                    val['tax_amount'] = currency.compute(val['amount'] * tax['ref_tax_sign'], company_currency, round=False)
+                    val['account_id'] = tax['account_paid_id'] or line.account_id.id
+                    val['account_analytic_id'] = tax['account_analytic_paid_id']
+
+                # If the taxes generate moves on the same financial account as the invoice line
+                # and no default analytic account is defined at the tax level, propagate the
+                # analytic account from the invoice line to the tax line. This is necessary
+                # in situations were (part of) the taxes cannot be reclaimed,
+                # to ensure the tax move is allocated to the proper analytic account.
+                if not val.get('account_analytic_id') and line.account_analytic_id and val['account_id'] == line.account_id.id:
+                    val['account_analytic_id'] = line.account_analytic_id.id
+
+                key = (val['tax_code_id'], val['base_code_id'], val['account_id'])
+                if not key in tax_grouped:
+                    tax_grouped[key] = val
+                else:
+                    tax_grouped[key]['base'] += val['base']
+                    tax_grouped[key]['amount'] += val['amount']
+                    tax_grouped[key]['base_amount'] += val['base_amount']
+                    tax_grouped[key]['tax_amount'] += val['tax_amount']
+
+        for t in tax_grouped.values():
+            t['base'] = currency.round(t['base'])
+            t['amount'] = currency.round(t['amount'])
+            t['base_amount'] = currency.round(t['base_amount'])
+            t['tax_amount'] = currency.round(t['tax_amount'])
+
+        return tax_grouped
+
+    @api.v7
+    def compute(self, cr, uid, voucher_id, context=None):
+        recs = self.browse(cr, uid, [], context)
+        invoice = recs.env['account.voucher'].browse(voucher_id)
+        return account_invoice_tax.compute(recs, invoice)
+
+    @api.model
+    def move_line_get(self, voucher_id):
+        res = []
+        self._cr.execute(
+            'SELECT * FROM account_invoice_tax WHERE voucher_id = %s',
+            (voucher_id,)
+        )
+        for row in self._cr.dictfetchall():
+            if not (row['amount'] or row['tax_code_id'] or row['tax_amount']):
+                continue
+            res.append({
+                'type': 'tax',
+                'name': row['name'],
+                'price_unit': row['amount'],
+                'quantity': 1,
+                'price': row['amount'] or 0.0,
+                'account_id': row['account_id'],
+                'tax_code_id': row['tax_code_id'],
+                'tax_amount': row['tax_amount'],
+                'account_analytic_id': row['account_analytic_id'],
+            })
+        return res
