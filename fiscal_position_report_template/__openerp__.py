346e01ee549651f49c14dc1762fc875ca1f27e29
Daniel Eriksson <daniel.eriksson@vertel.se>
2019-05-20 15:57:38 +0200

    fiscal_position_report_template: Added new module to print invoice depending on fiscal position

---
+++ fiscal_position_report_template/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2012-2016 Vertel (<http://www.vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Fiscal Position - Templates',
+    'version': '0.11',
+    'category': 'Localization/Account Charts',
+    'description': """""",
+    'author': 'Vertel',
+    'website': 'http://www.vertel.se',
+    'depends': ['account_payment', 'base_vat', 'account_chart'],
+    'init_xml': [],
+    'data': [
+        'fiscal_position_report.xml',
+    ],
+    'installable': 'True',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ fiscal_position_report_template/account.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2004-2016 Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api, models, fields, _
+import logging
+_logger = logging.getLogger(__name__)
+
+class account_fiscal_position(models.Model):
+    _inherit = 'account.fiscal.position'
+
+    report_default = fields.Many2one(comodel_name='ir.actions.report.xml', string='Default Report')
+
+
+class account_invoice(models.Model):
+    _name = "account.invoice"
+    _inherit = ['mail.thread']
+    
+    
+    @api.multi
+    def invoice_print(self):
+        """ Print the invoice and mark it as sent, so that we can see more
+            easily the next step of the workflow
+        """
+        assert len(self) == 1, 'This option should only be used for a single id at a time.'
+        self.sent = True
+        return self.env['report'].get_action(self, self.commercial_partner_id.property_account_position.report_default.report_name)
