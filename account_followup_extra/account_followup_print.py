438a4e96374f53bc77ac7a9de9af5347813d6703
Daniel Eriksson <daniel.eriksson@vertel.se>
2019-06-27 09:02:17 +0200

    account_followup_extra: Added module to allow exemption of prepaid invoices from follow up process

---
+++ account_followup_extra/account_followup_print.py
@@ -0,0 +1,110 @@
+ #-*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2019- Vertel AB.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning
+import datetime
+import time
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class sale_order(models.Model):
+    _inherit = "sale.order"
+
+    @api.onchange("payment_term")
+    def on_change_payment_term(self):
+        if self.payment_term == self.env.ref('account_followup_extra.payment_term_prepaid'):
+            self.order_policy = "prepaid"
+
+class account_payment_term(models.Model):
+    _inherit = "account.payment.term"
+
+    no_followup = fields.Boolean(string='No follow up', help='This option excludes invoices with this payment term from the follow up process.')
+
+class account_followup_print(models.Model):
+    _inherit = 'account_followup.print'
+
+    def _get_partners_followp(self, cr, uid, ids, context=None):
+        data = {}
+        data = self.browse(cr, uid, ids, context=context)[0]
+        company_id = data.company_id.id
+
+        cr.execute(
+            "SELECT l.partner_id, l.followup_line_id,l.date_maturity, l.date, l.id "\
+            "FROM account_move_line AS l "\
+                "LEFT JOIN account_account AS a "\
+                "ON (l.account_id=a.id) "\
+                "LEFT JOIN account_invoice AS i "\
+                "ON (l.move_id=i.move_id) "\
+                    "LEFT JOIN account_payment_term AS t "\
+                    "ON (i.payment_term=t.id) "\
+            "WHERE (l.reconcile_id IS NULL) "\
+                "AND (a.type='receivable') "\
+                "AND (l.state<>'draft') "\
+                "AND (l.partner_id is NOT NULL) "\
+                "AND (a.active) "\
+                "AND (l.debit > 0) "\
+                "AND (l.company_id = %s) "\
+                "AND (l.blocked = False) "\
+                "AND (t.no_followup is NOT True) "\
+            "ORDER BY l.date", (company_id,))  #l.blocked added to take litigation into account and it is not necessary to change follow-up level of account move lines without debit
+        move_lines = cr.fetchall()
+        old = None
+        fups = {}
+        fup_id = 'followup_id' in context and context['followup_id'] or data.followup_id.id
+        date = 'date' in context and context['date'] or data.date
+
+        current_date = datetime.date(*time.strptime(date,
+            '%Y-%m-%d')[:3])
+        cr.execute(
+            "SELECT * "\
+            "FROM account_followup_followup_line "\
+            "WHERE followup_id=%s "\
+            "ORDER BY delay", (fup_id,))
+        
+        #Create dictionary of tuples where first element is the date to compare with the due date and second element is the id of the next level
+        for result in cr.dictfetchall():
+            delay = datetime.timedelta(days=result['delay'])
+            fups[old] = (current_date - delay, result['id'])
+            old = result['id']
+
+        partner_list = []
+        to_update = {}
+        
+        #Fill dictionary of accountmovelines to_update with the partners that need to be updated
+        for partner_id, followup_line_id, date_maturity,date, id in move_lines:
+            if not partner_id:
+                continue
+            if followup_line_id not in fups:
+                continue
+            stat_line_id = partner_id * 10000 + company_id
+            if date_maturity:
+                if date_maturity <= fups[followup_line_id][0].strftime('%Y-%m-%d'):
+                    if stat_line_id not in partner_list:
+                        partner_list.append(stat_line_id)
+                    to_update[str(id)]= {'level': fups[followup_line_id][1], 'partner_id': stat_line_id}
+            elif date and date <= fups[followup_line_id][0].strftime('%Y-%m-%d'):
+                if stat_line_id not in partner_list:
+                    partner_list.append(stat_line_id)
+                to_update[str(id)]= {'level': fups[followup_line_id][1], 'partner_id': stat_line_id}
+        return {'partner_ids': partner_list, 'to_update': to_update}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
